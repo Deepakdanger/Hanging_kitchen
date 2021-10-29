26288fcf784274ea97348f5875e31032122e521f
Deepakdanger <depk.96.kumar@gmail.com>
2021-10-29 21:22:26 +0530

    Style the title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 const Navbar = () => (
   <nav className="nav1">
-    <p className="logo11"> Mammas Kitchen</p>
+    <div className="nav_title1">
+      <div className="nav_title2">
+        <div className="title">
+          <h1 className="pro">Han</h1>
+          <h1 className="glow">gin</h1>
+          <h1 className="pro">g</h1>
+        </div>
+        <div>
+          <h1 className="pro1 rotated">Kitchen</h1>
+        </div>
+      </div>
+    </div>
     <p className="right">
       <p className="data">Home</p>
       <p className="data">Search</p>
