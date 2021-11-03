94bd8b9c174d258e0bda7859bc6a09c9544bdbc6
Deepakdanger <depk.96.kumar@gmail.com>
2021-11-03 15:46:05 +0530

    Add positive and negative unit test for SelectCategoryReducer

---
+++ src/_tests_/reducer_test/Selectcategory.test.js
@@ -0,0 +1,27 @@
+import SelectCategoryReducer from '../../reducers/Selectcategory';
+import { SELECT_CATEGORY } from '../../actions/index';
+
+describe('SelectCategoryReducer function', () => {
+  it('should return the initial state', () => {
+    const action = { type: 'dummy_action' };
+    const initialState = '';
+    expect(SelectCategoryReducer(undefined, action)).toEqual(initialState);
+  });
+
+  it('should handle SELECT_CATEGORY', () => {
+    const action = {
+      type: SELECT_CATEGORY,
+      ele: { strCategory: 'Food' },
+    };
+    expect(SelectCategoryReducer({}, action) === '').toBe(false);
+  });
+
+  it('should handle SELECT_CATEGORY', () => {
+    const action = {
+      type: SELECT_CATEGORY,
+      ele: { strCategory: 'Food' },
+    };
+    const expectedState = 'Food';
+    expect(SelectCategoryReducer({}, action)).toEqual(expectedState);
+  });
+});
